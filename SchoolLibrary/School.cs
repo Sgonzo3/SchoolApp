77279aac2b3f359e0ad32fe0f3de86c003cae08b
Santos Gonzalez <sgonzo3@gmail.com>
2020-02-23 01:38:53 -0500

    feat: section 3.1 complete

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+            string SchoolName;
+            string SchoolAddress;
+            string SchoolCity;
+            string SchoolState;
+            string SchoolZip;
+            string PhoneNumber;
+
+        }
+    }
+}
