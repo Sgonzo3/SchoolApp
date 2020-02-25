feeec5d902af0390a3d99637ece25d2ace2b1d30
Santos Gonzalez <sgonzo3@gmail.com>
2020-02-24 21:38:51 -0500

    feat: section 3.4 creating properties

--- SchoolLibrary/School.cs
+++ SchoolLibrary/School.cs
@@ -12,6 +12,20 @@ namespace SchoolLibrary
             string Zip { get; set; }
             string PhoneNumber { get; set; }
 
+            string _twitterAddress;
+            string TwitterAddress
+            {
+                get { return _twitterAddress; }
+                set {
+                    if(value.StartsWith("@"))
+                    {
+                        _twitterAddress = value;
+                    } else
+                    {
+                        throw new Exception("The twitter address must begin with @");
+                    }
+                }
+            }
         }
     }
 }
